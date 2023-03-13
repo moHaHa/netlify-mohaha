8ee42c4d1c83fdceb9adb7b3d00382e0972fbe8b
HBO30 <diab.4869@gmail.com>
2023-03-13 21:25:34 +0200

    chore : init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    hi
+  </div>
+</template>
+<style>
+html {
+  background-color: #221;
+  color : white ;
+  color-scheme: dark;
+}
+</style>
